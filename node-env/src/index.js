9b6b8337d1a4ea5a1c4ee945b3db27cee21e2ead
Leonardo Casales <leonardo@smartnet.com.ar>
2021-01-21 22:28:23 -0300

    moment added

--- node-env/src/index.js
+++ node-env/src/index.js
@@ -1,5 +1,14 @@
 require('dotenv').config();
+const moment = require('moment'); // require
 
 const apiKey = process.env.GIFY_API_KEY;
 
 console.log("Api Key Gify:", apiKey);
+
+const hoy = moment( new Date() );
+
+const utc = moment( new Date() ).utc();
+console.log("hoy", hoy);
+console.log("utc", utc);
+
+console.log("diff", utc.diff(hoy));
